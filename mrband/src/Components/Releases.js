8a73288061f41247e28602d6d272bf8618f0a803
Ivan <calintz1@hotmail.com>
2020-11-02 12:10:57 -0300

    ajuste mobile

--- mrband/src/Components/Releases.js
+++ mrband/src/Components/Releases.js
@@ -4,7 +4,6 @@ import "./Releases.css";
 function Releases() {
   return (
     <div className="releases">
-      <div className="releases-title">Releases</div>
       <div className="releases-album">
         <img
           src={require("../Images/oldstreet.jpg")}
